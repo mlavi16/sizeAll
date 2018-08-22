d06866889ffaa2c9fc8282ecfceb1963b5f51608
mlavi16 <maya.lavi11@gmail.com>
2018-08-22 09:46:54 -0400

    commit

---
+++ js/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $(function(){
+        $('.fadein img:gt(0)').hide();
+        setInterval(function(){
+          $('.fadein :first-child').fadeOut()
+             .next('img').fadeIn()
+             .end().appendTo('.fadein');}, 
+          3000);
+    });
+});
